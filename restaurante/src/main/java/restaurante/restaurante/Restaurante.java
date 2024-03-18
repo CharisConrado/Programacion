5b82188e1a5eac6e00e3f070e2aa66068e9be105
CharisConrado <cmconradoc@ufpso.edu.co>
2024-03-18 11:16:42 -0500

    Restaurante

---
+++ restaurante/src/main/java/restaurante/restaurante/Restaurante.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package restaurante.restaurante;
+
+/**
+ *
+ * @author SCIS2PC26
+ */
+public class Restaurante {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
