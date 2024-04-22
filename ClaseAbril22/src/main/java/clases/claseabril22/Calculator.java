9909ff8ee36d664359cdd4163fbafc14b34a55bf
CharisConrado <cmconradoc@ufpso.edu.co>
2024-04-22 11:31:58 -0500

    Excepciones

---
+++ ClaseAbril22/src/main/java/clases/claseabril22/Calculator.java
@@ -0,0 +1,13 @@
+
+public class Calculator {
+    
+    public Calculator(){
+
+     }
+    
+    public double division(double a, double b){
+       // if (b<=0){
+             
+       //  System.out.print("Operaciones no permitidas");
+    }
+}
---
+++ ClaseAbril22/src/main/java/clases/claseabril22/ClaseAbril22.java
@@ -0,0 +1,52 @@
+
+package clases.claseabril22;
+
+public class ClaseAbril22 {
+
+    public static void main(String[] args) {
+        
+       Calculator calculator = new Calculator();
+    
+        try {
+            
+           double res = Calculator.division(4,2);
+           
+      } catch (Exception exception){ 
+          
+          System.out.print("Ha ocurrido un problema");
+          System.out.print(exception);
+          
+      } catch (InputMismatch){ 
+          
+          System.out.print("Ha ocurrido un problema");
+          System.out.print(exception);
+          
+      }
+        finally (){ 
+          
+          System.out.print("Ha ocurrido un problema");
+          System.out.print(exception);
+          
+      }
+                
+                try {
+                    int () numbers = new int // corechete 3;
+                            numbers // cochete 5 = 2/10;
+                          
+                } catch (arit{
+                    
+                
+                }
+       
+       
+    
+      
+       
+    }
+
+   
+
+   
+    
+}
+   
